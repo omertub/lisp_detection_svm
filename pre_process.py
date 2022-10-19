1e0714e9c0d1759a35e808cb1c7d81c9bd06f898
Omer Tubul <omerishn@gmail.com>
2022-10-19 22:47:49 +0300

    add snr

--- pre_process.py
+++ pre_process.py
@@ -1,4 +1,5 @@
-import sys, os, argparse
+import random
+import sys, os, argparse, math
 from scipy.io.wavfile import read
 from python_speech_features import mfcc
 from sklearn.preprocessing import normalize
@@ -97,11 +98,36 @@ def find_cut5(signal_r):
 
     return i
 
+def add_noise(samples):
+    noisy_samples = np.zeros_like(samples)
+    noise_factor=0.9
+    (frame_rate, noise) = read("noise.wav")
+    r = random.randint(0, len(noise) - len(samples))
+    for i in range(len(samples)):
+        noisy_samples[i]=noise_factor*samples[i]+(1-noise_factor)*noise[r+i]
+    return noisy_samples
 
-def generate_features(wav_file, word_data):
+def add_noise_snr(samples, snr_db):
+    (frame_rate, noise) = read("noise.wav")
+    r = random.randint(0, len(noise) - len(samples))
+    noise = noise[r:(r+len(samples))]
+
+    snr = 10**(snr_db/10)
+    Es = sum([(s**2) / len(samples) for s in samples])
+    En = sum([(s**2) / len(noise) for s in noise])
+    alpha = math.sqrt(Es/(snr*En))
+    noisy_signal = samples + alpha*noise
+    return noisy_signal
+
+
+def generate_features(wav_file, word_data, test, snr):
     (frame_rate, samples) = read(wav_file)
+    # if test and random.randint(0,1):
+    #     samples = add_noise(samples)
     samples_clean = remove_noise(samples)
     samples_n = normalize_samples(samples_clean)
+    if snr:
+        samples_n = add_noise_snr(samples_n, snr)
     
     cut = word_data.cut_func(samples_n)
 
@@ -121,14 +147,13 @@ def get_files_for_word(word_data, test):
         home_dir = "C:/Users/omer_/Desktop/recording_2/rec1"
     else:
         home_dir = "C:/Users/omer_/Desktop/recording_2/rec2"
-        home_dir = "C:/Users/omer_/Desktop/recording"
 
     result = [os.path.join(dp, f) for dp, dn, filenames in os.walk(home_dir) for f in filenames if os.path.splitext(f)[1] == '.wav']
-    files = [item.replace('\\','/') for item in result if word_data.word in item]
+    files = [item.replace('\\','/') for item in result if (word_data.word in item)]
     return files
 
 
-def extract_data_for_word(word_data, test):
+def extract_data_for_word(word_data, test, snr):
     X, y = np.array([]), np.array([])
 
     files = get_files_for_word(word_data, test)
@@ -139,9 +164,17 @@ def extract_data_for_word(word_data, test):
     for i in range(length):
         prec = (i/length) * 100
         print("%.2f" % prec, "%", end = '\r')
-        mfcc_feat = generate_features(files[i], word_data)
+        mfcc_feat = generate_features(files[i], word_data, test, snr)
         features = mfcc_feat.reshape(-1, 24)
         label = 1 if files[i][-6] == 'g' else 0
+        
+        # use for labels generation
+        # if (first):
+        #     final_array = np.array([label])
+        #     first = 0
+        # else:
+        #     final_array = np.concatenate((final_array, [label]))
+        # continue
 
         ## single_word mode
         if test:
@@ -190,18 +223,21 @@ def main():
     # argparse
     parser = argparse.ArgumentParser(description='Preprocess wav files')
     parser.add_argument('-w', '--word', help='Word num to visualize', type=int, required=True)
-    parser.add_argument('-ts', '--test', help='Generate test data')
+    parser.add_argument('-t', '--test', help='Generate test data', type=int)
+    parser.add_argument('-s', '--snr', help='Add noise with requested snr')
+
 
     args = parser.parse_args()
     word_data = WORDS[args.word]
 
-    if args.test:
+    test = bool(args.test)
+    if test:
         print("generating test")
     else:
         print("generating train")
 
-    word_info = extract_data_for_word(word_data, args.test)
-    if not args.test:
+    word_info = extract_data_for_word(word_data, test, args.snr)
+    if not test:
         np.savetxt(f'train/test_word.csv', word_info, delimiter=",")
 
 
